bb066a00b86544d6b22f03643f24d0a24e7c5100
paolaguacho <Paolacristina111@gmail.com>
2026-01-20 16:05:30 -0500

    Refactor UI test class name

--- src/test/java/com/paola/tests/ui/HomePageTests.java
+++ src/test/java/com/paola/tests/ui/HomePageTests.java
@@ -3,7 +3,7 @@ package com.paola.tests.ui;
 import com.paola.utilities.UiBaseTest;
 import org.junit.jupiter.api.Test;
 
-public class Testing extends UiBaseTest {
+public class HomePageTests extends UiBaseTest {
 
     @Test
     public void testing(){
