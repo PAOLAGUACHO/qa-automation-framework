696fd7b365eb3cda539a66d409f39b6a2c5878c8
paolaguacho <Paolacristina111@gmail.com>
2026-01-20 21:54:39 -0500

    HomePageTests added and UiBaseTest updated to POM

--- src/test/java/com/paola/tests/ui/HomePageTests.java
+++ src/test/java/com/paola/tests/ui/HomePageTests.java
@@ -1,12 +1,26 @@
 package com.paola.tests.ui;
 
+import com.paola.pages.HomePage;
+import com.paola.utilities.Driver;
 import com.paola.utilities.UiBaseTest;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class HomePageTests extends UiBaseTest {
 
     @Test
-    public void testing(){
+    public void homePage_shouldLoadSuccessfully() {
+        HomePage homePage = new HomePage(Driver.getDriver());
+
+        homePage.open();
+
+        assertTrue(homePage.isLoaded());
+
+        assertTrue(homePage.getSearchButton(wait).isDisplayed());
+
+        assertTrue(homePage.getViewDocsLink(wait).isDisplayed());
 
     }
 }
--- src/test/java/com/paola/utilities/UiBaseTest.java
+++ src/test/java/com/paola/utilities/UiBaseTest.java
@@ -1,23 +1,34 @@
 package com.paola.utilities;
 
+import dev.failsafe.internal.util.Durations;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static java.lang.Thread.sleep;
 
 public class UiBaseTest {
 
+    protected WebDriverWait wait;
+
     @BeforeAll
     public static void UI_Set_Up(){
         WebDriver driver = Driver.getDriver();
         driver.get("https://www.google.com/");
-
-
     }
 
-//    @AfterAll()
-//    public static void UI_Tear_Down(){
-//        Driver.closeDriver();
-//    }
+    @BeforeEach
+    public void WebDriverWait_Set_Up(){
+         wait= new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+    }
+
+    @AfterAll()
+    public static void UI_Tear_Down() throws InterruptedException {
+        sleep(3000);
+        Driver.closeDriver();
+    }
 }
